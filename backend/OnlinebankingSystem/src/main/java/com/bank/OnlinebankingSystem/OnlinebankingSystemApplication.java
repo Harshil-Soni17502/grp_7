d7f18e28a177259646ff87b81bf8e105823400d7
dhanpraja231 <dhanpraja231@gmail.com>
2023-08-09 13:49:43 +0530

    added spring

---
+++ backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/OnlinebankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bank.OnlinebankingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebankingSystemApplication.class, args);
+	}
+
+}
---
+++ backend/OnlinebankingSystem/src/test/java/com/bank/OnlinebankingSystem/OnlinebankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.OnlinebankingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
