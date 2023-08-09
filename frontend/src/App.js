0310421e23c8d67835a87e1a0a26a69b2ef19be9
dhanpraja231 <dhanpraja231@gmail.com>
2023-08-09 18:41:40 +0530

    react router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
+import Home from './component/home'
+import Login from './component/login'
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
 
 function App() {
   return (
+    <Router>
+
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -17,8 +24,23 @@ function App() {
         >
           Learn React
         </a>
+
+        <ul>
+          <li>
+            <Link to='/home'>Home</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+        </ul>
       </header>
     </div>
+    <Routes>
+      <Route exact path ='/' element ={<Home/>}></Route>
+      <Route exact path ='/home' element ={<Home/>}></Route>
+      <Route exact path ='/login' element ={<Login/>}></Route>
+    </Routes>
+    </Router>
   );
 }
 
