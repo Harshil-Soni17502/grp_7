844e26ae3f9e6607bd2cbc2cba6f2d32debdb6ee
Tushar Raj <tushar11raj@gmail.com>
2023-08-10 11:53:47 +0530

    added routers and login and register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import Login from "./Pages/Login";
+import Layout from "./Pages/Layout";
+import Register from "./Pages/Register";
+import Home from "./Pages/Home";
+import Error404 from "./Pages/Error404";
+import ReactDOM from "react-dom/client";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-     Online Banking System
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="*" element={<Error404 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
