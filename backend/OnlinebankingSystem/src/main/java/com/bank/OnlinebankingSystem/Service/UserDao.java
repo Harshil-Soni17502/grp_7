386ff6e3ae7e83e515999806fc1a1eb6adb2eb47
dhanpraja231 <dhanpraja231@gmail.com>
2023-08-10 19:21:56 +0530

    db fix1

--- backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/Service/UserDao.java
+++ backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/Service/UserDao.java
@@ -2,11 +2,9 @@ package com.bank.OnlinebankingSystem.Service;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import Model.User;
-
+import com.bank.OnlinebankingSystem.entity.User;
 
 @Repository
 public interface UserDao extends JpaRepository<User,Integer>{
-	
+
 }
