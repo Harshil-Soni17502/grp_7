5ff9dcb456b281a24fa1daef81ab1508eff1e135
unknown <hi.harshvardhan@gmail.com>
2023-08-11 00:05:48 +0530

    Added entity of user and account with constraints

--- backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/Entity/Account.java
+++ backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/Entity/Account.java
@@ -1,15 +1,23 @@
 package com.bank.OnlinebankingSystem.Entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "Account")
 public class Account {
+	@NotNull
     private String transactionPassword;
+	@NotNull
     private String accountType;
+	@NotNull
+	@Min(0)
     private Integer balance;
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
@@ -19,5 +27,38 @@ public class Account {
         this.accountType = accountType;
         this.balance = balance;
         this.id = id;
+    }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    @Column(name="password", nullable = false, unique=false, insertable=true, updatable=true)
+    public String getTransactionPassword() {
+        return transactionPassword;
+    }
+    public void setTransactionPassword(String transactionPassword) {
+        this.transactionPassword = transactionPassword;
+    }
+    
+    @Column(name="account_type", nullable=false, unique=false, insertable=true, updatable=true)
+    public String getAccountType() {
+    	return accountType;
+    }
+    public void setAccountType(String accountType) {
+    	this.accountType = accountType;
+    }
+    
+    @Column(name="balance", nullable=false, unique=false, insertable=true, updatable=true)
+    public Integer getBalance() {
+    	return balance;
+    }
+    public void setBalance(Integer balance) {
+    	this.balance = balance;
     }
 }
--- backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/Entity/User.java
+++ backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/Entity/User.java
@@ -1,24 +1,42 @@
 package com.bank.OnlinebankingSystem.Entity;
 
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "Users")
 public class User {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
+    @NotNull
     private String salutation;
-    @javax.validation.constraints.NotNull
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @NotNull
     private String emailId;
+    @NotNull
     private String password;
+    @NotNull
+    @Pattern(regexp="\d{10}")
     private String mobileNumber;
+    @NotNull
+    @Pattern(regexp="\d{12}")
     private String aadharNumber;
+    @NotNull
     private String permanentAddress;
+    @NotNull
     private String residentialAddress;
+    @NotNull
     private String dob;
+    @NotNull
     private String occupation;
 
 
@@ -50,8 +68,16 @@ public class User {
     public void setId(long id) {
         this.id = id;
     }
+    
+    @Column(name = "salutation", nullable = false, unique=false, insertable=true, updatable=true)
+    public String getSalutation() {
+        return salutation;
+    }
+    public void setSalutation(String salutation) {
+        this.salutation = salutation;
+    }
 
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "first_name", nullable = false, unique=false, insertable=true, updatable=true)
     public String getFirstName() {
         return firstName;
     }
@@ -59,7 +85,7 @@ public class User {
         this.firstName = firstName;
     }
 
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "last_name", nullable = false, unique=false, insertable=true, updatable=true)
     public String getLastName() {
         return lastName;
     }
@@ -67,11 +93,69 @@ public class User {
         this.lastName = lastName;
     }
 
-    @Column(name = "email_address", nullable = false)
+    @Column(name = "email_address", nullable = false, unique=true, insertable=true, updatable=true)
     public String getEmailId() {
         return emailId;
     }
     public void setEmailId(String emailId) {
         this.emailId = emailId;
     }
+    
+    @Column(name = "password", nullable = false, unique=false, insertable=true, updatable=true)
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    @Column(name = "mobile_number", nullable = false, unique=true, insertable=true, updatable=true)
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    
+    @Column(name = "aadhar_number", nullable = false, unique=true, insertable=true, updatable=true)
+    public String getAadharNumber() {
+        return aadharNumber;
+    }
+    public void setAadharNumber(String aadharNumber) {
+        this.aadharNumber = aadharNumber;
+    }
+    
+    @Column(name = "residential_address", nullable = false, unique=false, insertable=true, updatable=true)
+    public String getResidentialAddress() {
+    	return residentialAddress;
+    }
+    public void setResidentialAddress(String residentialAddress) {
+    	this.residentialAddress = residentialAddress;
+    }
+    
+    @Column(name = "permanent_address", nullable = false, unique=false, insertable=true, updatable=true)
+    public String getPermanentAddress() {
+    	return permanentAddress;
+    }
+    public void setPermanentAddress(String permanentAddress) {
+    	this.permanentAddress = permanentAddress;
+    }
+    
+    @Column(name = "dob", nullable = false, unique=false, insertable=true, updatable=true)
+    public String getDOB() {
+    	return dob;
+    }
+    public void setDOB(String dob) {
+    	this.dob = dob;
+    }
+    
+    @Column(name = "occupation", nullable = false, unique=false, insertable=true, updatable=true)
+    public String getOccupation() {
+    	return occupation;
+    }
+    public void setOccupation(String occupation) {
+    	this.occupation = occupation;
+    }
+    
+    
 }
