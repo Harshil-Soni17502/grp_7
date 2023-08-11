26d0a7215c538e40c348efaca4dc7b6d955488cc
dhanpraja231 <dhanpraja231@gmail.com>
2023-08-11 15:45:31 +0530

    added cors

---
+++ backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/Controller/TransactionController.java
@@ -0,0 +1,5 @@
+package com.bank.OnlinebankingSystem.Controller;
+
+public class TransactionController {
+
+}
---
+++ backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/Entity/Transaction.java
@@ -0,0 +1,5 @@
+package com.bank.OnlinebankingSystem.Entity;
+
+public class Transaction {
+
+}
