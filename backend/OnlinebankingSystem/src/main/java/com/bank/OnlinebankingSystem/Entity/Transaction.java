7718951677eb5f3109df401db9680eeda2fc57f0
dhanpraja231 <dhanpraja231@gmail.com>
2023-08-11 16:08:01 +0530

    check

--- backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/Entity/Transaction.java
+++ backend/OnlinebankingSystem/src/main/java/com/bank/OnlinebankingSystem/Entity/Transaction.java
@@ -1,12 +1,57 @@
 package com.bank.OnlinebankingSystem.Entity;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
-@Table(name="Transaction")
+//@Entity
+//@Table(name="Transaction")
 public class Transaction {
-	Integer amount;
-	String transactionType;
+	/*
+	 * private Integer amount; private String transactionType;
+	 * 
+	 * @GeneratedValue(strategy= GenerationType.AUTO) private Long id; private Date
+	 * transactionDate; private Account fromAccount; private Account toAccount;
+	 * 
+	 * public Transaction() {}
+	 * 
+	 * public Integer getAmount() { return amount; }
+	 * 
+	 * public void setAmount(Integer amount) { this.amount = amount; }
+	 * 
+	 * public String getTransactionType() { return transactionType; }
+	 * 
+	 * public void setTransactionType(String transactionType) { this.transactionType
+	 * = transactionType; }
+	 * 
+	 * @Id
+	 * 
+	 * @GeneratedValue(strategy = GenerationType.AUTO) public Long getId() { return
+	 * id; }
+	 * 
+	 * public void setId(Long id) { this.id = id; }
+	 * 
+	 * public Date getTransactionDate() { return transactionDate; }
+	 * 
+	 * public void setTransactionDate(Date transactionDate) { this.transactionDate =
+	 * transactionDate; }
+	 * 
+	 * public Account getFromAccount() { return fromAccount; }
+	 * 
+	 * public void setFromAccount(Account fromAccount) { this.fromAccount =
+	 * fromAccount; }
+	 * 
+	 * public Account getToAccount() { return toAccount; }
+	 * 
+	 * public void setToAccount(Account toAccount) { this.toAccount = toAccount; }
+	 */
+	
+	
+	
+	
 	
 }
