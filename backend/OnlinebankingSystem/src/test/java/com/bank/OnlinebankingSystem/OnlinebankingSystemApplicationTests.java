7761f0bab1e31f41e31a00f462a3fee808b04912
dhanpraja231 <dhanpraja231@gmail.com>
2023-08-24 13:19:55 +0530

    none

--- backend/OnlinebankingSystem/src/test/java/com/bank/OnlinebankingSystem/OnlinebankingSystemApplicationTests.java
+++ backend/OnlinebankingSystem/src/test/java/com/bank/OnlinebankingSystem/OnlinebankingSystemApplicationTests.java
@@ -1,13 +1,2 @@
-package com.bank.OnlinebankingSystem;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OnlinebankingSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+package com.bank.OnlinebankingSystem;public class OnlinebankingSystemApplicationTests {
 }
