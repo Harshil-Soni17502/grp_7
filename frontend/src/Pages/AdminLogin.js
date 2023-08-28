603403e8a050c877cb7931ee398259d7d8483906
dhanpraja231 <dhanpraja231@gmail.com>
2023-08-28 15:35:13 +0530

    removed forgot pwd buttton from admin login

--- frontend/src/Pages/AdminLogin.js
+++ frontend/src/Pages/AdminLogin.js
@@ -174,18 +174,18 @@ export default function AdminLogin() {
               >
                 Sign In
               </Button>
-              <Grid container>
+              {/* <Grid container>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
+                  {/* <Link href="#" variant="body2">
                     Forgot password?
-                  </Link>
+                  </Link> *
                 </Grid>
                 <Grid item>
                   <Link href="#" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
-              </Grid>
+              </Grid> */}
             </Box>
           </Box>
         </Grid>
